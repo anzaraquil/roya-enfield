d5a745c822caa3fb30311e55a24170b499569f87
anzaraquil <69158800+anzaraquil@users.noreply.github.com>
2022-06-02 14:44:59 +0530

    Add files via upload

---
+++ navBarElement.js
@@ -0,0 +1,6 @@
+const elem = document.querySelector('.fa-angle-down');
+elem.addEventListener('click',function(){
+	document.querySelector('.fa-angle-down').style.transform='rotate(180deg)';
+	
+
+})
